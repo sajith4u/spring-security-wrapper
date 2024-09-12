d8081197a236dd6ded13ccfdd7ca1d11555a3d7a
sajithv <sajith.vijesekara@gmail.com>
2024-09-12 12:55:16 +0530

    Initial Project

---
+++ src/main/java/com/mobiclabs/ssw/SpringSecurityWrapperApplication.java
@@ -0,0 +1,13 @@
+package com.mobiclabs.ssw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobiclabs/ssw/SpringSecurityWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobiclabs.ssw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
